f9892e7f6090f4732dd977c5b73b1517799d6260
retrax-agar <57232922+retrax-agar@users.noreply.github.com>
2019-10-31 16:11:30 -0300

    Add files via upload

---
+++ fbot.xanny.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         FBOT (Xanny Version)
+// @version      1.0.5
+// @author       Nebula & Nel
+// @match        *://agar.io/*
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+if (location.host === "agar.io" && location.pathname === "/") {
+    window.stop();
+    location.href = "https://agar.io/xanny" + location.hash;
+    return;
+}
+
+window.stop();
+document.documentElement.innerHTML = "";
+GM_xmlhttpRequest({
+    method: "GET",
+    url: "http://xn3bula.xyz/xanny/",
+    onload: function (e) {
+        document.open();
+        document.write(e.responseText);
+        document.close();
+    }
+});
